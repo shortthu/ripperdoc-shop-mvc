00e3493c2857899786470587eff494a5c42166fc
Thu Nguyen <thuthien2001@gmail.com>
2025-04-14 17:49:20 +0700

    chore: set up the project

---
+++ RipperdocShop/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RipperdocShop.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
