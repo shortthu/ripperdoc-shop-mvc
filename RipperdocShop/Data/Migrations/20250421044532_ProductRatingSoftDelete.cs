511601717428507861fb223386e23624d7ba2ce6
Thu Nguyen <thuthien2001@gmail.com>
2025-04-21 16:47:23 +0700

    fix: add migration for ProductRating soft delete

---
+++ RipperdocShop/Data/Migrations/20250421044532_ProductRatingSoftDelete.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RipperdocShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductRatingSoftDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "deleted_at",
+                table: "product_ratings",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "deleted_at",
+                table: "product_ratings");
+        }
+    }
+}
