5b96d7744eb8d89b5cc48ca8ec21190c5206c304
Thu Nguyen <thuthien2001@gmail.com>
2025-05-06 19:48:25 +0700

    fix: ProductRating is optional in DbSet for some reason. fixed

--- RipperdocShop.Api/Data/ApplicationDbContext.cs
+++ RipperdocShop.Api/Data/ApplicationDbContext.cs
@@ -17,7 +17,7 @@ public class ApplicationDbContext(
     public DbSet<Category> Categories { get; set; }
     public DbSet<Brand> Brands { get; set; }
     public DbSet<Product> Products { get; set; }
-    public DbSet<ProductRating?> ProductRatings { get; set; }
+    public DbSet<ProductRating> ProductRatings { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
     public DbSet<CartItem> CartItems { get; set; }
